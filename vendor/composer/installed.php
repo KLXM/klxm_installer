7ed945941d7685adf6660a44679e50c60e3288d0
Thomas Skerbis <thomas.skerbis@mac.com>
2026-06-05 20:50:04 +0200

    Paket mit vendor bereitstellen und config.php entfernen

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'klxm/installer-proxy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd5e1610a8a3a27ff64ea1c2caf6d33c958ed97bb',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'klxm/installer-proxy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd5e1610a8a3a27ff64ea1c2caf6d33c958ed97bb',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
